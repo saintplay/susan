41b749548e6345887c22feb85109fb2c9739a760
Diego Jara <saintplay96@gmail.com>
2017-06-07 03:37:37 -0500

    Cambia a directorios correctos staticos

--- src/main/java/com/example/demo/SusanApplication.java
+++ src/main/java/com/example/demo/SusanApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppacademyApplication {
-
+public class SusanApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AppacademyApplication.class, args);
+		SpringApplication.run(SusanApplication.class, args);
 	}
 }
